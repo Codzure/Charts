14783dfc60601d8fd521d439eb2ac9cad7344e44
Leonard Mutugi Muthoni <leonard.mutugi@equitybank.co.ke>
2022-12-21 00:08:57 +0300

    first commit

--- SwiftUI Charts/ContentView.swift
+++ SwiftUI Charts/ContentView.swift
@@ -5,15 +5,32 @@
 //  Created by Leonard Mutugi on 20/12/2022.
 //
 
+import Charts
 import SwiftUI
 
 struct ContentView: View {
+    
+    let weekdays = Calendar.current.shortWeekdaySymbols
+    let workouts = [45,65,75,15,22,30,35]
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Workout")
+                .font(.largeTitle)
+                .foregroundColor(.primary)
+                .padding()
+            Chart{
+                ForEach(weekdays.indices, id:\.self){
+                    idx in
+                    BarMark(x: .value("Day", weekdays[idx]), y: .value("workout(mins)", workouts[idx]))
+                        .foregroundStyle(by: .value("Day", weekdays[idx]))
+                        .annotation{
+                            Text("\(workouts[idx]) mins")
+                                .font(.caption)
+                        }
+                }
+            }
+            .frame(height: 400)
         }
         .padding()
     }
