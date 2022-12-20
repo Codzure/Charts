9c284c9f0a92a8810146b9a7e30936aabb65cb39
Leonard Mutugi Muthoni <leonard.mutugi@equitybank.co.ke>
2022-12-20 23:47:45 +0300

    Initial Commit

---
+++ SwiftUI Charts/SwiftUI_ChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ChartsApp.swift
+//  SwiftUI Charts
+//
+//  Created by Leonard Mutugi on 20/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
